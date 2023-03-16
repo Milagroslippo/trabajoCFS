a47204f864dcc7f658e563d0ad50f4c3cc0ff463
Milagroslippo <lippomilagros8@gmail.com>
2023-03-16 16:45:12 -0300

    plantar una planta

---
+++ HolaMundo.js
@@ -0,0 +1,7 @@
+console.log("plantar una planta");
+console.log("agarrar una pala");
+console.log("hacer un pozo");
+console.log("agarrar la planta");
+console.log("meter la planta en el pozo");
+console.log("tapar el pozo");
+console.log("regar");
